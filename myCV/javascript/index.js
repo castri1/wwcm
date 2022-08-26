b3cf17a2bf72e3885dbe6d05c08363d406039749
Antonieta13 <antonietaavellaneda@gmail.com>
2022-08-25 20:13:02 -0500

    lightMode

--- myCV/javascript/index.js
+++ myCV/javascript/index.js
@@ -19,14 +19,3 @@ function chistesAleatorios (){
         })
 }
 
-
-
-// modo dark and light
-
-const buttonChange=document.getElementById("button-change");
-buttonChange.addEventListener("click",changeColor)
-
-function changeColor(){
-    let html=document.querySelector("html")
-    html.classList.toggle("dark");
-}
